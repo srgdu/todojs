698dc8d3f6f544aa2648fbf71e4a4eb2531d4059
Sergei <srgdude7@gmail.com>
2019-10-31 21:22:33 +0200

    Second commit

--- js/app.js
+++ js/app.js
@@ -1,107 +1,107 @@
+const main = (() => {
+	// Imperative style
 
-// Imperative style
+	function createElement (tag, props, ...children) {
+		const element = document.createElement(tag);
 
-// Function returns DOM element
-function createTodoItem (title) {
-	const checkbox = document.createElement('input');
-	checkbox.type = 'checkbox';
-	checkbox.className = 'checkbox';
+		Object.keys(props).forEach(key => element[key] = props[key]);
 
-	const label = document.createElement('label');
-	label.innerText = title;
-	label.className = 'title';
+		if (children.length > 0) {
+			children.forEach(child => {
+				if (typeof child == 'string')
+					child = document.createTextNode(child);
 
-	const editInput = document.createElement('input');
-	editInput.type = 'text';
-	editInput.className = 'textfield';
+				element.appendChild(child);
+			})
+		}
 
-	const editButton = document.createElement('button');
-	editButton.innerText = 'Изменить';
-	editButton.className = 'edit';
-
-	const deleteButton = document.createElement('button');
-	deleteButton.innerText = 'Удалить';
-	deleteButton.className = 'delete';
-
-	const listItem = document.createElement('li');
-	listItem.className = 'todo-item';
-
-	listItem.appendChild(checkbox);
-	listItem.appendChild(label);
-	listItem.appendChild(editInput);
-	listItem.appendChild(editButton);
-	listItem.appendChild(deleteButton);
-
-	bindEvents(listItem);
-
-	return listItem;
-}
-
-
-function bindEvents (todoItem) {
-	const checkbox = todoItem.querySelector('.checkbox');
-	const editButton = todoItem.querySelector('button.edit');
-	const deleteButton = todoItem.querySelector('button.delete');
-
-	checkbox.addEventListener('change', toggleTodoItem);
-	editButton.addEventListener('click', editTodoItem);
-	deleteButton.addEventListener('click', deleteTodoItem);
-}
-
-
-function addTodoItem (event) {
-	event.preventDefault();
-
-	if (addInput.value === '') return alert('You must type your task.');
-
-	const todoItem = createTodoItem(addInput.value);
-	todoList.appendChild(todoItem);
-	addInput.value = '';
-}
-
-
-function toggleTodoItem () {
-	const listItem = this.parentNode;
-	listItem.classList.toggle('completed');
-}
-
-
-function editTodoItem () {
-	const listItem = this.parentNode;
-	const title = listItem.querySelector('.title');
-	const editInput = listItem.querySelector('.textfield');
-	const isEditing = listItem.classList.contains('editing');
-
-	if (isEditing) {
-		title.innerText = editInput.value;
-		this.innerText = 'Изменить';
-	} else {
-		editInput.value = title.innerText;
-		this.innerText = 'Сохранить';
+		return element;
 	}
 
-	listItem.classList.toggle('editing');
-}
+
+	function createTodoItem (title) {
+		const checkbox = createElement('input', { type: 'checkbox', className: 'checkbox' });
+		const label = createElement('label', { className: 'title' }, title);
+		const editInput = createElement('input', { type: 'text', className: 'textfield' });
+		const editButton = createElement('button', { className: 'edit' }, 'Изменить');
+		const deleteButton = createElement('button', { className: 'delete' }, 'Удалить');
+
+		const listItem = createElement('li', { className: 'todo-item' }, checkbox, label, editInput, editButton, deleteButton);
+
+		bindEvents(listItem);
+
+		return listItem;
+	}
 
 
-function deleteTodoItem () {
-	const listItem = this.parentNode;
-	todoList.removeChild(listItem);
-}
+	function bindEvents (todoItem) {
+		const checkbox = todoItem.querySelector('.checkbox');
+		const editButton = todoItem.querySelector('button.edit');
+		const deleteButton = todoItem.querySelector('button.delete');
+
+		checkbox.addEventListener('change', toggleTodoItem);
+		editButton.addEventListener('click', editTodoItem);
+		deleteButton.addEventListener('click', deleteTodoItem);
+	}
 
 
-const todoForm = document.getElementById('todo-form');
-const addInput = document.getElementById('add-input');
-const todoList = document.getElementById('todo-list');
-const todoItems = document.querySelectorAll('.todo-item');
+	function addTodoItem (event) {
+		event.preventDefault();
+
+		if (addInput.value === '') return alert('You must type your task.');
+
+		const todoItem = createTodoItem(addInput.value);
+		todoList.appendChild(todoItem);
+		addInput.value = '';
+	}
 
 
-function main () {
+	function toggleTodoItem () {
+		const listItem = this.parentNode;
+		listItem.classList.toggle('completed');
+	}
+
+
+	function editTodoItem () {
+		const listItem = this.parentNode;
+		const title = listItem.querySelector('.title');
+		const editInput = listItem.querySelector('.textfield');
+		const isEditing = listItem.classList.contains('editing');
+
+		if (isEditing) {
+			title.innerText = editInput.value;
+			this.innerText = 'Изменить';
+		} else {
+			editInput.value = title.innerText;
+			this.innerText = 'Сохранить';
+		}
+
+		listItem.classList.toggle('editing');
+	}
+
+
+	function deleteTodoItem () {
+		const listItem = this.parentNode;
+		todoList.removeChild(listItem);
+	}
+
+
+	const todoList = document.getElementById('todo-list');
+	const addInput = document.getElementById('add-input');
+	const todoForm = document.getElementById('todo-form');
+	const todoItems = document.querySelectorAll('.todo-item');
+
+
+	function main () {
+		
+		todoForm.addEventListener('submit', addTodoItem);
+		bindEvents(todoList);
+	}
+
+
+	return main;
 	
-	todoForm.addEventListener('submit', addTodoItem);
-	bindEvents(todoList);
-}
-
+})(document);
 
 main();
 
